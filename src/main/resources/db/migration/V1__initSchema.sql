972114d1ab6cada0e9e389bbdcb914094779cfd0
Andrzej <andrzej.przybysz01@gmail.com>
2021-03-12 23:15:07 +0100

    update db migration

--- src/main/resources/db/migration/V1__initSchema.sql
+++ src/main/resources/db/migration/V1__initSchema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.directory (
+CREATE TABLE directory (
     id bigint NOT NULL,
     created timestamp without time zone,
     modified timestamp without time zone,
@@ -6,20 +6,16 @@ CREATE TABLE public.directory (
     parent_id bigint
 );
 
-ALTER TABLE public.directory OWNER TO file_manager;
-
-CREATE SEQUENCE public.directory_id_seq
+CREATE SEQUENCE directory_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
-ALTER TABLE public.directory_id_seq OWNER TO file_manager;
+ALTER SEQUENCE directory_id_seq OWNED BY directory.id;
 
-ALTER SEQUENCE public.directory_id_seq OWNED BY public.directory.id;
-
-CREATE TABLE public.resource_file (
+CREATE TABLE resource_file (
     id bigint NOT NULL,
     created timestamp without time zone,
     extension character varying(255),
@@ -30,36 +26,31 @@ CREATE TABLE public.resource_file (
     parent_directory_id bigint
 );
 
-ALTER TABLE public.resource_file OWNER TO file_manager;
-
-CREATE SEQUENCE public.resource_file_id_seq
+CREATE SEQUENCE resource_file_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
 
-ALTER TABLE public.resource_file_id_seq OWNER TO file_manager;
+ALTER SEQUENCE resource_file_id_seq OWNED BY resource_file.id;
 
-ALTER SEQUENCE public.resource_file_id_seq OWNED BY public.resource_file.id;
+ALTER TABLE ONLY directory ALTER COLUMN id SET DEFAULT nextval('directory_id_seq'::regclass);
 
-ALTER TABLE ONLY public.directory ALTER COLUMN id SET DEFAULT nextval('public.directory_id_seq'::regclass);
+ALTER TABLE ONLY resource_file ALTER COLUMN id SET DEFAULT nextval('resource_file_id_seq'::regclass);
 
-ALTER TABLE ONLY public.resource_file ALTER COLUMN id SET DEFAULT nextval('public.resource_file_id_seq'::regclass);
+SELECT pg_catalog.setval('directory_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.directory_id_seq', 1, false);
+SELECT pg_catalog.setval('resource_file_id_seq', 1, false);
 
-SELECT pg_catalog.setval('public.resource_file_id_seq', 1, false);
-
-ALTER TABLE ONLY public.directory
+ALTER TABLE ONLY directory
     ADD CONSTRAINT directory_pkey PRIMARY KEY (id);
 
-ALTER TABLE ONLY public.resource_file
+ALTER TABLE ONLY resource_file
     ADD CONSTRAINT resource_file_pkey PRIMARY KEY (id);
 
+ALTER TABLE ONLY directory
+    ADD CONSTRAINT fkeqcjnap90heiiqqfon2shtao0 FOREIGN KEY (parent_id) REFERENCES directory(id);
 
-ALTER TABLE ONLY public.directory
-    ADD CONSTRAINT fkeqcjnap90heiiqqfon2shtao0 FOREIGN KEY (parent_id) REFERENCES public.directory(id);
-
-ALTER TABLE ONLY public.resource_file
-    ADD CONSTRAINT fkre37s8ib77jm8rs2ux2i9s39r FOREIGN KEY (parent_directory_id) REFERENCES public.directory(id);
+ALTER TABLE ONLY resource_file
+    ADD CONSTRAINT fkre37s8ib77jm8rs2ux2i9s39r FOREIGN KEY (parent_directory_id) REFERENCES directory(id);
